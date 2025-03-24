7eab8795b1dac6664a5472b65c97c41865fcf3d1
duddnddnfl <duddnddnfl@naver.com>
2025-03-25 01:17:41 +0900

    의존성 추가

--- core/src/main/java/com/example/notification/entity/Notification.java
+++ core/src/main/java/com/example/notification/entity/Notification.java
@@ -1,21 +1,15 @@
 package com.example.notification.entity;
 
-
 import com.example.notification.type.NotificationType;
+import lombok.AllArgsConstructor;
+
 import java.time.Instant;
 
+@AllArgsConstructor
 public class Notification {
     public String id;
     public Long userId;
     public NotificationType notificationType;
     public Instant createdAt;
     public Instant deletedAt;
-
-    public Notification(String id, Long userId, NotificationType notificationType, Instant createdAt, Instant deletedAt) {
-        this.id = id;
-        this.userId = userId;
-        this.notificationType = notificationType;
-        this.createdAt = createdAt;
-        this.deletedAt = deletedAt;
-    }
 }
