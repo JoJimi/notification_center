f524aaf8bf950a3dad629a9e12fe8422dc2b6719
duddnddnfl <duddnddnfl@naver.com>
2025-03-29 17:07:30 +0900

    댓글 작성 이벤트 처리 구현

--- consumer/src/main/java/org/example/task/CommentAddTask.java
+++ consumer/src/main/java/org/example/task/CommentAddTask.java
@@ -1,6 +1,5 @@
 package org.example.task;
 
-import com.NotificationIdGenerator;
 import com.example.*;
 import lombok.extern.slf4j.Slf4j;
 import org.example.event.comment.CommentEvent;
--- core/src/main/java/com/example/NotificationIdGenerator.java
+++ core/src/main/java/com/example/NotificationIdGenerator.java
@@ -1,4 +1,4 @@
-package com;
+package com.example;
 
 import org.bson.types.ObjectId;
 
