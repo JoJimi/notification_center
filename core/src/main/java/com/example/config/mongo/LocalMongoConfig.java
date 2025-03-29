c77ab86eda9f54593ee6a49a5cd93c24b5469e50
duddnddnfl <duddnddnfl@naver.com>
2025-03-29 23:14:54 +0900

    mongodb 설정하기

--- core/src/main/java/com/example/config/mongo/LocalMongoConfig.java
+++ core/src/main/java/com/example/config/mongo/LocalMongoConfig.java
@@ -12,19 +12,19 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@Profile("test")
 @Slf4j
 @Configuration
 public class LocalMongoConfig {
 
     private static final String MONGODB_IMAGE_NAME = "mongo:5.0";
-    private static final int MONGODB_INNER_PORT = 27017;
+    private static final Integer MONGODB_INNER_PORT = 27017;
     private static final String DATABASE_NAME = "notification";
     private static final GenericContainer mongo = createMongoInstance();
 
     private static GenericContainer createMongoInstance() {
         return new GenericContainer(DockerImageName.parse(MONGODB_IMAGE_NAME))
-                .withExposedPorts(MONGODB_INNER_PORT);
+                .withExposedPorts(MONGODB_INNER_PORT)
+                .withReuse(true);
     }
 
     @PostConstruct
