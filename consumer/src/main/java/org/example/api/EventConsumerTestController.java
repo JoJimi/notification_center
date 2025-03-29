ddc7eb0a28acbef48bead1ddcbb6ad3c78fdf761
duddnddnfl <duddnddnfl@naver.com>
2025-03-29 20:28:46 +0900

    댓글 부분 테스트 진행

---
+++ consumer/src/main/java/org/example/api/EventConsumerTestController.java
@@ -0,0 +1,22 @@
+package org.example.api;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.example.event.comment.CommentEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Consumer;
+
+@RestController
+public class EventConsumerTestController implements EventConsumerTestControllerSpec{
+
+    @Autowired
+    private Consumer<CommentEvent> comment;
+
+    @Override
+    @PostMapping("/test/comment")
+    public void comment(@RequestBody CommentEvent event){
+        comment.accept(event);
+    }
+}
