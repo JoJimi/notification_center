5558b999beacb0510eb55eb7c85f033a198ba775
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 15:54:13 +0900

    팔로우 이벤트 DTO 만들기

--- consumer/src/main/java/com/example/event/follow/FollowEvent.java
+++ consumer/src/main/java/com/example/event/follow/FollowEvent.java
@@ -7,7 +7,7 @@ import java.time.Instant;
 @Data
 public class FollowEvent {
     private FollowEventType type;
-    private long userId;
-    private long targetUserId;
+    private Long userId;
+    private Long targetUserId;
     private Instant createdAt;
 }
