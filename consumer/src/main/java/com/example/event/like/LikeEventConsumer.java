db39557dc321d3373c93ecb89989bc948aa2b67f
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 16:28:02 +0900

    좋아요 이벤트 로그 수정

--- consumer/src/main/java/com/example/event/like/LikeEventConsumer.java
+++ consumer/src/main/java/com/example/event/like/LikeEventConsumer.java
@@ -1,17 +1,28 @@
 package com.example.event.like;
 
+import com.example.task.like.LikeAddTask;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
+import static com.example.event.like.LikeEventType.*;
+
 @Slf4j
 @Component
 public class LikeEventConsumer {
 
+    @Autowired
+    private LikeAddTask likeAddTask;
+
     @Bean("like")
     public Consumer<LikeEvent> like() {
-        return event -> log.info(event.toString());
+        return event -> {
+            if(event.getType() == ADD){
+                likeAddTask.processEvent(event);
+            }
+        };
     }
 }
