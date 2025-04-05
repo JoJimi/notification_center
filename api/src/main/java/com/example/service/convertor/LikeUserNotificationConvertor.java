f204564390cc34fd91335aeb85d4711d0b124c41
duddnddnfl <duddnddnfl@naver.com>
2025-04-05 17:46:18 +0900

    좋아요 관련 Convertor 생성

---
+++ api/src/main/java/com/example/service/convertor/LikeUserNotificationConvertor.java
@@ -0,0 +1,36 @@
+package com.example.service.convertor;
+
+import com.example.client.PostClient;
+import com.example.client.UserClient;
+import com.example.domain.comment.CommentNotification;
+import com.example.domain.like.LikeNotification;
+import com.example.domain.post.Post;
+import com.example.domain.user.User;
+import com.example.service.dto.ConvertedCommentNotification;
+import com.example.service.dto.ConvertedLikeNotification;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LikeUserNotificationConvertor {
+
+    private final UserClient userClient;
+    private final PostClient postClient;
+
+    public ConvertedLikeNotification convert(LikeNotification notification){
+        User user = userClient.getUser(notification.getLikerIds().getLast());
+        Post post = postClient.getPost(notification.getPostId());
+
+        return new ConvertedLikeNotification(
+                notification.getId(),
+                notification.getType(),
+                notification.getOccurredAt(),
+                notification.getLastUpdateAt(),
+                user.getName(),
+                user.getProfileImageUrl(),
+                notification.getLikerIds().size(),
+                post.getImageUrl()
+        );
+    }
+}
--- api/src/main/java/com/example/service/dto/ConvertedLikeNotification.java
+++ api/src/main/java/com/example/service/dto/ConvertedLikeNotification.java
@@ -9,11 +9,11 @@ import java.time.Instant;
 public class ConvertedLikeNotification extends ConvertedNotification{
     private final String userName;
     private final String userProfileImageUrl;
-    private final Long userCount;
+    private final long userCount;
     private final String postImageUrl;
 
     public ConvertedLikeNotification(String id, NotificationType type, Instant occurredAt, Instant lastUpdatedAt,
-                                     String userName, String userProfileImageUrl, Long userCount, String postImageUrl) {
+                                     String userName, String userProfileImageUrl, long userCount, String postImageUrl) {
         super(id, type, occurredAt, lastUpdatedAt);
         this.userName = userName;
         this.userProfileImageUrl = userProfileImageUrl;
