c43e3dd3042704b00d2efa956b484ca22bb68736
duddnddnfl <duddnddnfl@naver.com>
2025-04-05 18:25:22 +0900

    사용자 Response DTO 생성

--- api/src/main/java/com/example/service/dto/response/user/UserNotificationListResponse.java
+++ api/src/main/java/com/example/service/dto/response/user/UserNotificationListResponse.java
@@ -1,5 +1,6 @@
 package com.example.service.dto.response.user;
 
+import com.example.service.dto.GetUserNotificationsResult;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,5 +14,20 @@ public class UserNotificationListResponse {
     private List<UserNotificationResponse> notifications;
     private boolean hasNext;
     private Instant pivot;
+
+    public static UserNotificationListResponse of(GetUserNotificationsResult result){
+        List<UserNotificationResponse> notifications = result.getNotifications().stream()
+                .map(UserNotificationResponse::of)
+                .toList();
+
+        Instant pivot = (result.isHasNext() && !notifications.isEmpty())
+                ? notifications.getLast().getOccurredAt() : null;
+
+        return new UserNotificationListResponse(
+                notifications,
+                result.isHasNext(),
+                pivot
+        );
+    }
 
 }
--- api/src/main/java/com/example/service/dto/response/user/UserNotificationResponse.java
+++ api/src/main/java/com/example/service/dto/response/user/UserNotificationResponse.java
@@ -1,6 +1,13 @@
 package com.example.service.dto.response.user;
 
 import com.example.domain.notification.NotificationType;
+import com.example.service.dto.convert.ConvertedCommentNotification;
+import com.example.service.dto.convert.ConvertedFollowNotification;
+import com.example.service.dto.convert.ConvertedLikeNotification;
+import com.example.service.dto.convert.ConvertedNotification;
+import com.example.service.dto.response.comment.CommentUserNotificationResponse;
+import com.example.service.dto.response.follow.FollowUserNotificationResponse;
+import com.example.service.dto.response.like.LikeUserNotificationResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,9 +15,18 @@ import java.time.Instant;
 
 @Getter
 @AllArgsConstructor
-public class UserNotificationResponse {
+public abstract class UserNotificationResponse {
 
     private String id;
     private NotificationType type;
     private Instant occurredAt;
+
+    public static UserNotificationResponse of(ConvertedNotification notification) {
+        switch (notification.getType()){
+            case COMMENT -> { return CommentUserNotificationResponse.of((ConvertedCommentNotification) notification); }
+            case LIKE -> { return LikeUserNotificationResponse.of((ConvertedLikeNotification) notification); }
+            case FOLLOW -> { return FollowUserNotificationResponse.of((ConvertedFollowNotification) notification); }
+            default -> throw new IllegalArgumentException("Unsupported notification type: " + notification.getType());
+        }
+    }
 }
