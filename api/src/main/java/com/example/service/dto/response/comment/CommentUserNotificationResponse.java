5d3c04362527b07505c044aa5c6484e8c3ce019d
duddnddnfl <duddnddnfl@naver.com>
2025-04-05 18:12:24 +0900

    댓글 Response DTO 생성

---
+++ api/src/main/java/com/example/service/dto/response/comment/CommentUserNotificationResponse.java
@@ -0,0 +1,38 @@
+package com.example.service.dto.response.comment;
+
+import com.example.domain.notification.NotificationType;
+import com.example.service.dto.ConvertedCommentNotification;
+import com.example.service.dto.response.user.UserNotificationResponse;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+public class CommentUserNotificationResponse extends UserNotificationResponse {
+
+    private final String userName;
+    private final String userProfileImageUrl;
+    private final String comment;
+    private final String postImageUrl;
+
+    public CommentUserNotificationResponse(String id, NotificationType type, Instant occurredAt, String userName,
+                                           String userProfileImageUrl, String comment, String postImageUrl) {
+        super(id, type, occurredAt);
+        this.userName = userName;
+        this.userProfileImageUrl = userProfileImageUrl;
+        this.comment = comment;
+        this.postImageUrl = postImageUrl;
+    }
+
+    public static CommentUserNotificationResponse of(ConvertedCommentNotification notification){
+         return new CommentUserNotificationResponse(
+                 notification.getId(),
+                 notification.getType(),
+                 notification.getOccurredAt(),
+                 notification.getUserName(),
+                 notification.getUserProfileImageUrl(),
+                 notification.getComment(),
+                 notification.getPostImageUrl()
+         );
+    }
+}
