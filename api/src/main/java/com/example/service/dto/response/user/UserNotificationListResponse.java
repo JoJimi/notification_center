5fbafaebfba1906431a69368feeb1cb9521668de
duddnddnfl <duddnddnfl@naver.com>
2025-04-05 17:59:41 +0900

    유저 알림 Response DTO 생성

---
+++ api/src/main/java/com/example/service/dto/response/user/UserNotificationListResponse.java
@@ -0,0 +1,17 @@
+package com.example.service.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UserNotificationListResponse {
+
+    private List<UserNotificationListResponse> notifications;
+    private boolean hasNext;
+    private Instant pivot;
+
+}
