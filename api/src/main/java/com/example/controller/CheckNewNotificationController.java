28699521c893f971ee663f89a70676eecc5a7f2e
duddnddnfl <duddnddnfl@naver.com>
2025-04-06 20:52:25 +0900

    신규 알림 여부 조회 API 구현

---
+++ api/src/main/java/com/example/controller/CheckNewNotificationController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.dto.response.CheckNewNotificationResponse;
+import com.example.service.CheckNewNotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/v1/user-notifications")
+public class CheckNewNotificationController {
+
+    private final CheckNewNotificationService service;
+
+    @GetMapping("/{userId}/new")
+    public CheckNewNotificationResponse checkNew(
+            @PathVariable(value = "userId") Long userId
+    ){
+        boolean hasNew = service.checkNewNotification(userId);
+        return new CheckNewNotificationResponse(hasNew);
+    }
+}
